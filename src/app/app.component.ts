04bb7c86e2588e8cd34f182014f09d5e122bc91f
Fred <frehd.syc88@gmail.com>
2019-11-17 18:43:08 -0500

    User services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'api-demo';
+
+  model = {
+    left: true,
+    middle: false,
+    right: false
+  };
+
+  users: any[] = [];
+
+  constructor(
+    protected userService: UserService
+  ) { }
+
+  ngOnInit() {
+    this.userService.getUsers()
+    .subscribe(
+      (data) => {
+        this.users = data['results'];
+      },
+      (error) => {
+        console.error(error);
+      }
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
+
+import { UserService } from './user.service';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
